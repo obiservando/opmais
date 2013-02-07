32dc8b43cf7ecfdfb976892c451e1f960c959064
obiserv@live.com <obiserv@live.com@5808051a-7402-b914-24f8-678789e95708>
2013-02-07 14:47:11 +0000

    formatação

--- trunk/redirect.js
+++ trunk/redirect.js
@@ -1,7 +1,6 @@
 var d=document; 
 
-if (d.forms.length<7){
-window.location.replace(d.getElementById('comments')/
-.previousSibling.previousSibling.previousSibling.previousSibling./
-previousSibling.previousSibling.getElementsByTagName('p')[0].getElementsByTagName('a')[0].href);
+if (d.forms.length<7)
+{
+window.location.replace(d.getElementById('comments').previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.getElementsByTagName('p')[0].getElementsByTagName('a')[0].href);
 };
