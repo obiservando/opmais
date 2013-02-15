4a43f774b355b9e215864df1d223a75e562b7fb4
obiserv@live.com <obiserv@live.com@5808051a-7402-b914-24f8-678789e95708>
2013-02-15 19:52:34 +0000

    display no final

--- trunk/os.js
+++ trunk/os.js
@@ -6,7 +6,7 @@ document.getElementById('footer').style.display='none';
 document.getElementsByTagName('h2')[0].style.display='none';
 document.getElementsByTagName('h1')[0].style.display='none';
 var d=document.getElementById('wrapper').getElementsByTagName('div'), i=0; for (;i<d.length;){ if(d[i].getAttribute("class")!=null){ var i2 , a=['counts','sidebar','headSingle','boxFontSize']; for (i2 in a){ if(d[i].getAttribute("class")==a[i2]){d[i].style.display='none';}}} i++}
-document.body.style.display='block';
+
 
 function listagem(fx) {
 fx.contentWindow.document.getElementById('wrapper').onclick=function(){ int=setInterval(function(){var d=fx.contentWindow.document.getElementById('wrapper').getElementsByTagName('img'), i=0; for (;i<d.length;){ if(d[i].getAttribute("class")!=null){ if(d[i].getAttribute("class").indexOf ('user-7268')!=-1){d[i].parentNode.parentNode.style.display='none';}} i++}},1000); int2=setTimeout(function(){int=clearInterval(int);},10000);}
@@ -20,4 +20,5 @@ fx.contentWindow.document.getElementsByTagName('h2')[0].style.display='none';
 fx.contentWindow.document.getElementsByTagName('h1')[0].style.display='none';
 var d=fx.contentWindow.document.getElementById('wrapper').getElementsByTagName('div'), i=0; for (;i<d.length;){ if(d[i].getAttribute("class")!=null){ var i2 , a=['counts','sidebar']; for (i2 in a){ if(d[i].getAttribute("class")==a[i2]){d[i].style.display='none';}}} i++}
 fx.style.display='block';
+document.body.style.display='block';
 }
