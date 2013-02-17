eb29286863adbe7769c7e731a74a6085f65598a4
obiserv@live.com <obiserv@live.com@5808051a-7402-b914-24f8-678789e95708>
2013-02-17 01:26:57 +0000

    header show

--- trunk/os.js
+++ trunk/os.js
@@ -1,6 +1,6 @@
 document.head.appendChild(document.createElement('base'))
 document.getElementsByTagName('base')[0].target='_blank'
-document.getElementById('header').style.display='none';
+//document.getElementById('header').style.display='none';
 document.getElementById('footer').style.display='none';
 document.getElementsByTagName('h2')[0].style.display='none';
 document.getElementsByTagName('h1')[0].style.display='none';
@@ -13,7 +13,7 @@ fx.contentWindow.document.getElementById('wrapper').click()
 
 fx.contentWindow.document.head.appendChild(document.createElement('base'));
 fx.contentWindow.document.getElementsByTagName('base')[0].target='_blank'
-//fx.contentWindow.document.getElementById('header').style.display='none';
+fx.contentWindow.document.getElementById('header').style.display='none';
 var d=fx.contentWindow.document.getElementById('wrapper').getElementsByTagName('div'), i=0; for (;i<d.length;){ if(d[i].getAttribute("class")!=null){ var i2 , a=['counts','sidebar']; for (i2 in a){ if(d[i].getAttribute("class")==a[i2]){d[i].style.display='none';}}} i++}
 fx.contentWindow.document.getElementsByTagName('h1')[0].style.display='none';
 fx.contentWindow.document.getElementsByTagName('h2')[0].style.display='none';
