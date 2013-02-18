99e659cf28701085f2bdfeb1fb97d9f37bfb1a2e
obiserv@live.com <obiserv@live.com@5808051a-7402-b914-24f8-678789e95708>
2013-02-18 04:06:02 +0000

    37

--- trunk/sd.js
+++ trunk/sd.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  if (location.href=="http://www.observadorpolitico.org.br/"){
+  if (location.href.length==37){
   if($(".dosOPs").html()==null){
 $(".boxDiscussoesFeatured").after('<div class="boxDiscussoesFeatured dosOPs"><h2><span>Destaque dos Observadores:</span></h2><div  style="height:117px;width:736px;overflow:hidden;"><iframe style="position:relative;left:-6px;top:-1px;"  src="https://docs.google.com/presentation/d/1ILEJ9yEL8a0z-QZvJnJaiozXRu7XYTtBsb8_48Xbg3Q/embed?start=true&loop=true&delayms=10000" frameborder="0" width="748" height="148" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" ></iframe></div></div>');
   };
