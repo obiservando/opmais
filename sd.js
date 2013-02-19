89a26b1d5851979117d75972ead78296eec72704
obiserv@live.com <obiserv@live.com@5808051a-7402-b914-24f8-678789e95708>
2013-02-19 14:02:52 +0000

    subst #abc9

--- sd.js
+++ sd.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 
   if (location.href.length==37){
   if($(".dosOPs").html()==null){
-$(".boxDiscussoesFeatured").after('<div class="boxDiscussoesFeatured dosOPs"><h2><span>Destaque dos Observadores:</span></h2><div  style="height:117px;width:736px;overflow:hidden;"><iframe style="position:relative;left:-6px;top:-1px;"  src="https://docs.google.com/presentation/d/1ILEJ9yEL8a0z-QZvJnJaiozXRu7XYTtBsb8_48Xbg3Q/embed?start=true&loop=true&delayms=10000" frameborder="0" width="748" height="148" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" ></iframe></div></div>');
+//$(".boxDiscussoesFeatured").after  
+$(".#abc9").after('<div class="boxDiscussoesFeatured dosOPs"><h2><span>Destaque dos Observadores:</span></h2><div  style="height:117px;width:736px;overflow:hidden;"><iframe style="position:relative;left:-6px;top:-1px;"  src="https://docs.google.com/presentation/d/1ILEJ9yEL8a0z-QZvJnJaiozXRu7XYTtBsb8_48Xbg3Q/embed?start=true&loop=true&delayms=10000" frameborder="0" width="748" height="148" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" ></iframe></div></div>');
   };
  };
   
