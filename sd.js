14d56c32117a304a16ab9a4e0a84ea36e44dcd7a
obiserv@live.com <obiserv@live.com@5808051a-7402-b914-24f8-678789e95708>
2013-02-24 02:40:24 +0000

    jquery efects

--- sd.js
+++ sd.js
@@ -2,7 +2,46 @@ $(document).ready(function(){
 
   if (location.href.length==37){
  
-$(".post:has('#abc9')").html('<a href="http://www.observadorpolitico.org.br/grupos/tecnologia/forum/topic/opsimple"><img src="http://www.observadorpolitico.org.br/wp-content/uploads/avatars/9089/95618b3923ca9bfd0475f11193c59813-bpfull.jpg"alt=""class="avatar user-7268-avatar"width="60"height="60"></a><div class="text"><p class="cat">Opinião</p><h3><a href="http://www.observadorpolitico.org.br/grupos/tecnologia/forum/topic/opsimple">Destaque dos Observadores:</a></h3><marquee direction="up"scrolldelay="100"onmouseover="this.scrollAmount=0"onmouseout="this.scrollAmount=1"scrollamount="1"style="height: 90px;width:100%"><iframe style="overflow:hidden;border:none; width:100%;height:400px"src="http://widget.allourideas.org/destaque_dos_observadores/results?locale=pt"scrolling="no"></iframe></marquee><p class="metas"><a href="http://www.observadorpolitico.org.br/observadores/destacam"class="author">Por Destaque dos Observadores</a>, <span class="data">'+ new Date().toLocaleDateString()+' às '+ new Date().toLocaleTimeString()+'</span><a href="http://www.observadorpolitico.org.br/grupos/tecnologia/forum/topic/opsimple"class="comments"title="Destaque dos Observadores:"> / vote &bull; opine &bull; ideia &bull; bate-papo</a></p></div><div class="clear"></div>');
+       th = document.getElementById('abc9').parentNode.parentNode.parentNode.parentNode;
+        $(th).hide().html('<div></div><div class="text"><p class="metas"><a href="http://www.observadorpolitico.org.br/grupos/tecnologia/forum/topic/opsimple"class="author">Destaque dos Observadores</a>, <span class="data">' + new Date().toLocaleDateString() + ' &agrave;s ' + new Date().toLocaleTimeString() + '</span><a href="http://www.observadorpolitico.org.br/grupos/tecnologia/forum/topic/opsimple"class="comments"title="Destaque dos Observadores"> / vote &bull; opine &bull; ideia &bull; bate-papo</a></p></div><div class="clear"></div></div>');
+        $(th.firstChild).load("http://www.observadorpolitico.org.br/grupos/brasil/forum/topic/seguiram-todas-as-regras-fizeram-o-que-lhes-foi-ordenado-o-que-deu-errado .textSingle p:first",
+        function(responseTxt, statusTxt, xhr) {
+            if (statusTxt == "success") {
+                var t = $(th.firstChild).text().split('\n');
+                var i;
+                $(th.firstChild).html('');
+                for (i in t) {
+                    $(th.firstChild).append('<div></div>');
+                    $(th.firstChild.childNodes[i]).load('http://www.observadorpolitico.org.br/?s=' + encodeURI(t[i]) + ' .post')
+                };
+                ls = th.firstChild.childNodes;
+                a = 0;
+                b = 0;
+                $(ls).hide();
+                $(ls[b]).show();
+                try {
+                    int = clearInterval(int)
+                } catch(err) {}
+                si();
+                $(ls).mouseover(function() {
+                    int = clearInterval(int)
+                });
+                $(ls).mouseout(function() {
+                    si()
+                });
+                $(th).show();
+                function si() {
+                    int = setInterval(function() {
+                        $(ls[b]).hide();
+                        a++;
+                        $(ls[a]).fadeIn("slow");
+                        b = a;
+                        if (a > ls.length - 2) a = -1
+                    },
+                    5000)
+                }
+            }
+        })
 
  }
   else {
