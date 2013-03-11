902806467a938791805a7239e2f3c6302ba307b8
obiserv@live.com <obiserv@live.com@5808051a-7402-b914-24f8-678789e95708>
2013-03-11 18:13:48 +0000

    simplificada

--- sd.js
+++ sd.js
@@ -3,42 +3,19 @@ if (document.getElementById('abc9')!=null) {
  
           $(document).ready(function() {
         th = document.getElementById('abc9').parentNode.parentNode.parentNode.parentNode;
-        $(th).hide().html('<div></div><div class="text"><p class="metas"><a href="http://www.observadorpolitico.org.br/grupos/tecnologia/forum/topic/opsimple"class="author">Destaque dos Observadores</a>, <span class="data">' + new Date().toLocaleDateString() + ' &agrave;s ' + new Date().toLocaleTimeString() + '</span><a href="http://www.observadorpolitico.org.br/grupos/tecnologia/forum/topic/opsimple"class="comments"title="Destaque dos Observadores"> / vote &bull; opine &bull; ideia &bull; bate-papo</a></p></div><div class="clear"></div></div>');
-        $(th.firstChild).load("http://www.observadorpolitico.org.br/grupos/comunicacao/forum/topic/votar-1 .textSingle p:first",
+        $(th).hide().html('<div></div>');
+        $(th.firstChild).load("http://www.observadorpolitico.org.br/grupos/brasil/forum/topic/%E1%8D%A7-a-questao%E2%9D%A2/ .textSingle p:first",
         function(responseTxt, statusTxt, xhr) {
             if (statusTxt == "success") {
                 var t = $(th.firstChild).text().split('\n');
                 var i;
                 $(th.firstChild).html('');
                 for (i in t) {
-                    $(th.firstChild).append('<div></div>');
-                    $(th.firstChild.childNodes[i]).load('http://www.observadorpolitico.org.br/?s=' + encodeURI(t[i]) + ' .post')
+                    $(th).before($('<div></div>').load('http://www.observadorpolitico.org.br/?s=' + encodeURI(t[i]) + ' .post'));
+                    
                 };
-                ls = th.firstChild.childNodes;
-                a = 0;
-                b = 0;
-                $(ls).hide();
-                $(ls[b]).show();
-                function si() {
-                    int = setInterval(function() {
-                        $(ls[b]).hide();
-                        a++;
-                        $(ls[a]).fadeIn("slow");
-                        b = a;
-                        if (a > ls.length - 2) a = -1
-                    }, 5000);
-                };
-                try {
-                    int = clearInterval(int)
-                } catch(err) {}
-                si();
-                $(ls).mouseover(function() {
-                    int = clearInterval(int);
-                });
-                $(ls).mouseout(function() {
-                    si();
-                });
-                $(th).show();
+                
+
                 
                 
             };
